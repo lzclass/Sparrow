0abe66fa60eb3f275da282e4cb3a82955120959e
liuzhao <63993048@qq.com>
2019-01-18 18:04:46 +0800

    重新覆盖，之前的编译不过去

--- baselib/src/main/java/com/moving/keep/baselib/utils/RouterPath.java
+++
@@ -1,6 +0,0 @@
-package com.moving.keep.baselib.utils;
-
-public class RouterPath {
-    public static final String MODULE_ONE = "module/one";
-    public static final String MAIN_ACTIVITY = "module/main";
-}
