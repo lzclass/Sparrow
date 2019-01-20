956aab7c326c182e4bb0c3a37204c442b9bfd4c8
lzclass <63993048@qq.com>
2019-01-20 23:01:53 +0800

    study

---
+++ module_service/src/main/java/com/example/module_service/MyAccessServicer.java
@@ -0,0 +1,16 @@
+package com.example.module_service;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class MyAccessServicer extends AccessibilityService {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
