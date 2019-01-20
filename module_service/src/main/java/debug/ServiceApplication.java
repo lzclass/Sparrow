36e75ec82db32be3cf195491b0f0eb88714bbf32
lzclass <63993048@qq.com>
2019-01-20 23:13:21 +0800

    study

---
+++ module_service/src/main/java/debug/ServiceApplication.java
@@ -0,0 +1,13 @@
+package debug;
+
+
+import com.guiying.module.common.base.BaseApplication;
+
+public class ServiceApplication extends BaseApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
